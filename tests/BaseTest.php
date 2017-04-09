f49b5f3c27fc2b4142685a862e9f878bbacd5f06
zhangwei <942178655@qq.com>
2017-04-10 03:15:01 +0800

    重写

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -1,31 +1,62 @@
 <?php
 
-namespace zhiyicx\Component\QiniuOSS\Tests;
+include_once "../vendor/autoload.php";
 
-use zhiyicx\Component\QiniuOSS\QiniuOssStream;
+
+use zhiyicx\Component\QiniuOSS\QiniuOSS;
 
 /**
- * 基础测试.
- *
- **/
-class BaseTest extends \PHPUnit_Framework_TestCase
+* BaseTest
+*/
+class BaseTest extends QiniuOSS
 {
-    const OSS_BUCKET = '28youth';
-    const OSS_ACCESS_KEY_ID = 'UEwud1dLTn5Qn0swqpvKhhvUt2GJBvNvg6agr9sA';
-    const OSS_ACCESS_KEY_SECRET = 'T4IwE_3uPW_bHRoa_L1up8sS58krW7vKxCogo6hl';
-
-    protected $oss;
+    const BUCKET = '28youth';
+    const DOMAIN = 'omt6lyv55.bkt.clouddn.com';
+    const ACCESS_KEY = 'UEwud1dLTn5Qn0swqpvKhhvUt2GJBvNvg6agr9sA';
+    const SECRET_KEY = 'T4IwE_3uPW_bHRoa_L1up8sS58krW7vKxCogo6hl';
 
     public function __construct()
     {
-        $this->oss = new QiniuOssStream();
+        parent::__construct(self::ACCESS_KEY, self::SECRET_KEY, self::BUCKET, self::DOMAIN);
     }
 
+    public function testopen()
+    {
+        $f = fopen('oss://text/txt1.txt', 'r');
 
- 
+        // return  fstat($f); //获取信息
+        return $f;
+    }
+
+    public function testRead()
+    {
+        return  fread($this->testopen(), 1000);
+    }
+
+    public function testPut()
+    {
+        // return fwrite($this->testopen(), fread(fopen('D:\txt2.txt', 'r'), 1000));
+        return fwrite(fopen('oss://t2.txt', 'w'), file_get_contents('D:\txt2.txt'));
+    }
+
+    /* 底层正则不明白 */
+    public function testOpendir()
+    {
+        $list =  opendir('oss://text');
+
+        return $list;
+    }
+
+    /* 有点问题 */
+    public function testRename()
+    {
+        $o = rename('oss://text/t1.txt', 'oss://text/t3.txt');
+
+        return $o;
+    }
 }
 
-
-$test = new BaseTest();
-
-print_r($test);
+$oss = new BaseTest();
+$oss->registerStreamWrapper('oss');
+echo "<pre>";
+print_r($oss->testPut());
